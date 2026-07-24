edb03dbc251fb23773cd91152aadd68f69b5c961
TiaraBasori <mintabyss233@gmail.com>
2026-07-24 06:11:38 +0000

    Remove offline account login restriction (auto-applied after upstream sync)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/account/CreateAccountPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/account/CreateAccountPane.java
@@ -97,10 +97,6 @@ public class CreateAccountPane extends JFXDialogLayout implements DialogAware {
 
     public CreateAccountPane(AccountFactory<?> factory) {
         if (factory == null) {
-            if (AccountListPage.RESTRICTED.get()) {
-                showMethodSwitcher = false;
-                factory = Accounts.FACTORY_MICROSOFT;
-            } else {
                 showMethodSwitcher = true;
                 String preferred = settings().preferredLoginTypeProperty().get();
                 try {
@@ -108,7 +104,6 @@ public class CreateAccountPane extends JFXDialogLayout implements DialogAware {
                 } catch (IllegalArgumentException e) {
                     factory = Accounts.FACTORY_OFFLINE;
                 }
-            }
         } else {
             showMethodSwitcher = false;
         }
